a652b9bd7d63472639bec50ec5d5cf1a0a06fd63
muraleo <muraleo80923@gmail.com>
2017-07-14 21:17:33 -0400

    exercise 2.3.1, 2.3.2

--- 1_createTable/create_table_practice.sql
+++ 1_createTable/create_table_practice.sql
@@ -41,3 +41,42 @@ ALTER TABLE PRINTER DROP color; /*remember ; */
 be 'none' if the laptop does not have an optical disk.*/
 
 ALTER TABLE LAPTOP ADD od CHAR(5) DEFAULT 'none';
+
+/*Classes(class, type, country, numGuns, bore, displacement)
+Ships(name, class, launched)
+Battles(name, date)
+Outcomes(ship, battle, result)*/
+
+CREATE TABLE CLASSES(
+	class		VARCHAR(50) PRIMARY KEY,
+	type		CHAR(2),
+	country		VARCHAR(100),
+	numGuns	INT,
+	bore		DECIMAL(6,2),
+	displacement	DECIMAL(10,2)
+)
+
+CREATE TABLE SHIPS(
+	name		VARCHAR(50),
+	class                VARCHAR(50),
+	launched	DATE,
+	PRIMARY KEY	(name, class),
+)
+
+CREATE TABLE BATTLES(
+	name		VARCHAR(50) PRIMARY KEY,
+	date 		DATE
+)
+
+CREATE TABLE OUTCOMES(
+	ship 		VARCHAR(50),
+	battle 		VARCHAR(50),
+	result 		CHAR(10),
+	UNIQUE (ship, battle)
+)
+
+/*An alteration to your Classes relation from (a) to delete the attribute bore.*/
+ALTER TABLE CLASSES DROP bore;
+
+/*An alteration to your Ships relation from (b) to include the attribute yard giving the shipyard where the ship was built. */
+ALTER TABLE SHIPS yard VARCHAR(100);
